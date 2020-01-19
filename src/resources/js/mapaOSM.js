468e78a376c3ccd2dd038fdd1288a6c1b7ff7d43
josego <josego85@gmail.com>
2020-01-19 17:00:49 -0300

    Se agrega informacion principal.

--- src/resources/js/mapaOSM.js
+++ src/resources/js/mapaOSM.js
@@ -39,19 +39,19 @@ function loadMap()
         }).addTo(map);
     });
 
-    var layer_scientist;
-    var greenIcon = new L.Icon(
+    var layer_working_paraguayans;
+    var icon = new L.Icon(
     {
-        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png',
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
     });
-    $.getJSON("datos/scientist_py.geojson", function(data_scientist)
+    $.getJSON("datos/working_paraguayans.geojson", function(data_working_paraguayans)
     {
-        layer_scientist = L.geoJson(data_scientist,
+        layer_working_paraguayans = L.geoJson(data_working_paraguayans,
         {
             onEachFeature: onEachFeature,
             pointToLayer: function(feature, latlng)
@@ -59,7 +59,7 @@ function loadMap()
                 return L.marker(latlng,
                 {
                     title: feature.properties.name, 
-                    icon: greenIcon
+                    icon: icon
                 });
 			}
 		}).addTo(map);
